754e974c8ee4189c4d08707fc9cb3955de535e6f
Paul cartau <pcartau@student.42.fr>
2019-08-18 17:04:08 +0200

    maj index.js

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,11 +1,13 @@
-let question = document.getElementById('question');
-let askButton = document.getElementById('askButton');
-let newAnswer = document.getElementById('newAnswer');
-let newAnswerButton = document.getElementById('confirmAnswer');
-let answerText = document.getElementById('answer');
+let DOM = {
+    question : document.getElementById('question'),
+    askButton : document.getElementById('askButton'),
+    newAnswer : document.getElementById('newAnswer'),
+    newAnswerButton : document.getElementById('confirmAnswer'),
+    answerText : document.getElementById('answer')
+};
 
-async function sendQA(question, answer, route) {
-    const rawResponse = await fetch(route, {
+function sendQA(question, answer, route) {
+    return rawResponse = fetch(route, {
         method: 'POST',
         headers: {
         'Accept': 'application/json',
@@ -15,27 +17,22 @@ async function sendQA(question, answer, route) {
             question: question,
             answer: answer
         })
-    });
-    const response = await rawResponse.json();
-    return (response);
+    })
+    .then(response => response.json());
 };
 
-askButton.addEventListener("click", async () => {
-    let answer = await sendQA(question.value, undefined, '/sendQuestion');
-    if (answer) {
-        answerText.innerText = answer;
-        newAnswer.style.display = "none";
-        newAnswerButton.style.display = "none";
-    } else {
-        answerText.innerText = "No answer found ! You can provide an answer to the question";
-        newAnswer.style.display = "flex";
-        newAnswerButton.style.display = "flex";
-    }
+askButton.addEventListener("click", () => {
+    sendQA(question.value, undefined, '/sendQuestion')
+    .then(answer => {
+        DOM.answerText.innerText = answer ? answer : "No answer found ! You can provide an answer to the question";
+        DOM.newAnswer.style.display = answer ? "none" : "flex";
+        DOM.newAnswerButton.style.display = answer ? "none" : "flex";
+    })
 });
 
-newAnswerButton.addEventListener("click", async () => {
+DOM.newAnswerButton.addEventListener("click", async () => {
     await sendQA(question.value, newAnswer.value, '/createQuestion');
-    newAnswer.style.display = "none";
-    newAnswerButton.style.display = "none";
-    answerText.innerText = "Answer added with success !"
+    DOM.newAnswer.style.display = "none";
+    DOM.newAnswerButton.style.display = "none";
+    DOM.answerText.innerText = "Answer added with success !"
 });
